79356a16beee93329b2fb07bcd7a3327049ae99f
LINUX Users Group BPDC <lugbpdc@dubai.bits-pilani.ac.in>
2025-08-04 18:57:44 +0000

    Switch to static export (free plan) and remove SSR features

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,11 +1,3 @@
 
-import {https} from "firebase-functions";
-import next from "next";
-
-const dev = process.env.NODE_ENV !== "production";
-const app = next({dev, conf: {distDir: ".next"}});
-const handle = app.getRequestHandler();
-
-export const nextApp = https.onRequest((req, res) => {
-  return app.prepare().then(() => handle(req, res));
-});
+// Intentionally left blank.
+// Add your Cloud Functions logic here.
